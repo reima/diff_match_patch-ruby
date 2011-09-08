965aba9f00473ad1c646dac7a770451729f7a080
Matthias Reitinger <reitinge@in.tum.de>
2011-09-08 21:02:35 +0200

    Initial commit

---
+++ diff_patch_match_test.rb
@@ -0,0 +1,20 @@
+require 'test/unit'
+require_relative 'diff_patch_match'
+
+class DiffTest < Test::Unit::TestCase
+  def setup
+    @dmp = DiffPatchMatch.new
+  end
+
+  def test_diff_commonPrefix
+    # Detect any common prefix
+    # Null case.
+    assert_equal(0, @dmp.diff_commonPrefix('abc', 'xyz'))
+
+    # Non-null case.
+    assert_equal(4, @dmp.diff_commonPrefix('1234abcdef', '1234xyz'))
+
+    # Whole case.
+    assert_equal(4, @dmp.diff_commonPrefix('1234', '1234xyz'))
+  end
+end

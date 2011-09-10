2817972a5620e3767f5fca7e25c6a863934046a5
Matthias Reitinger <reitinge@in.tum.de>
2011-09-10 22:04:23 +0200

    Removed duplicate definition of diff_commonPrefix

--- diff_match_patch.rb
+++ diff_match_patch.rb
@@ -88,29 +88,6 @@ class DiffPatchMatch
     return 0 if text1.empty? || text2.empty? || text1[0] != text2[0]
 
     # Binary search.
-    pointer_min = 0
-    pointer_max = [text1.length, text2.length].min
-    pointer_mid = pointer_max
-    pointer_start = 0
-    while pointer_min < pointer_mid
-      if text1[pointer_start...pointer_mid] == text2[pointer_start...pointer_mid]
-        pointer_min = pointer_mid
-        pointer_start = pointer_min
-      else
-        pointer_max = pointer_mid
-      end
-      pointer_mid = (pointer_max - pointer_min) / 2 + pointer_min
-    end
-
-    return pointer_mid
-  end
-
-  # Determine the common prefix of two strings.
-  def diff_commonPrefix(text1, text2)
-    # Quick check for common null cases.
-    return 0 if text1.empty? || text2.empty? || text1[0] != text2[0]
-
-    # Binary search.
     # Performance analysis: http://neil.fraser.name/news/2007/10/09/
     # See diff_patch_match_bench.rb
     pointer_min = 0
@@ -138,6 +115,7 @@ class DiffPatchMatch
 
     # Binary search.
     # Performance analysis: http://neil.fraser.name/news/2007/10/09/
+    # See diff_patch_match_bench.rb
     pointer_min = 0
     pointer_max = [text1.length, text2.length].min
     pointer_mid = pointer_max
